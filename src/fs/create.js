feda8b0fc0e53bb5a242a0b25ee232c5c2948052
Andrei <platonovaudit@yandex.ru>
2022-11-29 09:38:12 +0500

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { appendFile, stat } from 'node:fs/promises';
+import { join } from 'node:path';
+
+const filePath = join("./files/", "fresh.txt");
+const data = `I am fresh and young`;
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const itemStats = await stat(filePath);
+    } catch {
+        await appendFile(filePath, data);
+        return;
+    }
+    throw Error("FS operation failed");
 };
 
 await create();
