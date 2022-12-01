e1146c4b72e824e755eba2fb9e7378bd5591c8ae
Andrei <platonovaudit@yandex.ru>
2022-12-01 13:06:54 +0500

    feat: solve task rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as giveProperName } from 'node:fs/promises';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const folderPath = dirname(fileURLToPath(import.meta.url));
+const folderName = "files";
+const wrongName = "wrongFilename.txt";
+const properName = "properFilename.md";
+const wrongPath = join(folderPath, folderName, wrongName);
+const properPath = join(folderPath, folderName, properName);
+
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        await giveProperName(wrongPath, properPath);
+    } catch {
+        throw new Error("FS operation failed");
+    }
 };
 
 await rename();
