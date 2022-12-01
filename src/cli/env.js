2496197397a4951a223d496400d0e9da43e0badc
Andrei <platonovaudit@yandex.ru>
2022-12-01 14:09:19 +0500

    feat: solve cli and read tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,6 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = Object.entries(process.env).filter(item => item[0].startsWith("RSS_")).map(item => `${item[0]}=${item[1]}`);
+    console.log(envVariables.join("; "));
 };
 
 parseEnv();
