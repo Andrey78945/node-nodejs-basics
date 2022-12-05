1d8d317a05e5b79e6d902185554426e6c3fca649
Andrei <platonovaudit@yandex.ru>
2022-12-05 18:18:40 +0500

    feat: solve child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fork } from 'node:child_process';
+
+const folderPath = dirname(fileURLToPath(import.meta.url));
+const folderName = "files";
+const fileName = "script.js";
+const filePath = join(folderPath, folderName, fileName);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = fork(filePath, args);
+    child.send(process.stdin);
+
+    child.on("message", message => process.stdout);
+    child.on("error", error => process.stdout);
 };
 
 spawnChildProcess();
